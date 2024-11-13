7438e51be0f916520245f29d64f468c93b9cfa1a
nadira-23 <nadiramaheen23@gmail.com>
2024-11-13 13:30:56 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Email from './components/Email'
+import Welcome from './components/Welcome'
+import { Route, Router, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Email/>
-        
+      
+            <Routes>
+                <Route path="/" element={<Email />} />
+                <Route path="/welcome" element={<Welcome/>} />
+            </Routes>
+       
     </>
   )
 }
---
+++ frontend/src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Welcome() {
+    return (
+        <div style={{ padding: '2rem' }}>
+            <h1>Welcome to the Dashboard!</h1>
+            <p>You have successfully logged in.</p>
+        </div>
+    );
+}
+
+export default Welcome;
